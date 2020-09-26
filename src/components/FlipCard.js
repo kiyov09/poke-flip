c3ee1c0d7976a2f7bc21405a2ecd7e5a25952f05
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2020-09-26 12:07:09 -0500

    Update FlipCard styles

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -32,7 +32,7 @@ const FlipCard = ({ info, flipped, className, onClick, index, disabled }) => {
                 <div className="flipCard__side flipCard__side--front bg-blue-800 border-solid border-white border-4 rounded-md h-full w-full absolute shadow-2xl flex items-center justify-center ">
                     <img src="./images/Poke_Ball_Vector.png" alt="Pokeball" />
                 </div>
-                <div className="flipCard__side flipCard__side--back bg-blue-300 h-full w-full absolute shadow-2xl flex items-center justify-center rounded-md ">
+                <div className="flipCard__side flipCard__side--back bg-gray-300 border-solid border-gray-200 border-4 h-full w-full absolute shadow-2xl flex items-center justify-center rounded-md ">
                     <img src={ info.image } alt="value" />
                 </div>
             </div>
